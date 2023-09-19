2ccaab464e103a00c9258c1b2e60fad5d0f95455
Edipo Luis Federle <edipofederle@gmail.com>
2023-09-19 19:08:19 -0300

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to rinha-compiler
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/rinha_compiler/core_test.clj
@@ -0,0 +1,7 @@
+(ns rinha-compiler.core-test
+  (:require [clojure.test :refer :all]
+            [rinha-compiler.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
